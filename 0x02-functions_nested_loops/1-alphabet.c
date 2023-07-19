5ddf5da3bdf42af304e3ea53ea2422199b0d885f
ahmdgm <ahmedgamalgamal1@gmail.com>
2023-07-18 19:00:05 -0700

    change

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphapet - print_alphapets_in_lower_case
+ */
+
+void print_alphabet(void)
+{
+	char alphapet;
+	for (alphapet = 'a'; alphapet <= 'z'; alphapet++)
+		_putchar(alphapet);
+	_putchar('\n');
+}
