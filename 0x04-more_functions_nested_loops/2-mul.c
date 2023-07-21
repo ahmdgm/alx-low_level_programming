0ab80d84918234e9434a7a15ac3836982cb489dc
ahmdgm <ahmedgamalgamal1@gmail.com>
2023-07-20 17:29:02 -0700

    change

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 "include "main.h"
 
 /**
- * mul - function that multiplies two integers
- * @a: first digit
- * @b: second digit
- * Return: multiplication of a * b
- */
+* mul - function that multiplies two integers
+* @a: first digit
+* @b: second digit
+* Return: multiplication of a * b
+*/
 
 int mul(int a, int b)
 {
