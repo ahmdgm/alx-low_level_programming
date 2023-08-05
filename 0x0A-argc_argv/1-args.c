692de2b458480273b41dd861eafeb2d043839e6a
ahmdgm <ahmedgamalgamal1@gmail.com>
2023-08-04 17:34:32 -0700

    change

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h" 
+#include <stdio.h>
+/**
+* main - program to print number of arguments 
+* @argc: number of the arguments
+* @argv: array of pointer to char 
+*/
+
+int main(int argc, cahr **argv __attribute__((unused)))
+{
+	printf("%d\n",argc - 1);
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - main program
+* @argc: number of arguments ,
+* @argv: array of pointer to char
+* Return: the int converted from the string
+*/
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
