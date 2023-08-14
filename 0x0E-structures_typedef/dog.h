530f5f7f298c536cf1e479b10bbff6275398ceca
ahmdgm <ahmedgamalgamal1@gmail.com>
2023-08-14 08:28:30 -0700

    change

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,9 +20,6 @@ struct dog
 */
 typedef struct dog dog_t;
 
-/**
-* prototypes
-*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
