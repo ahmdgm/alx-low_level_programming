7253508411158df4ba09b5629533789f26a5560e
ahmdgm <ahmedgamalgamal1@gmail.com>
2023-08-14 07:22:38 -0700

    change

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - Define a new type struct dog
+* @name: string
+* @age: float
+* @owner: string
+* Description: Define a new type struct dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
