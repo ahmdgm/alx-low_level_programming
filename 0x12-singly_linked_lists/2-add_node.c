cfb0ce1e5590a96ae29a335de1116370e8f3f595
ahmdgm <ahmedgamalgamal1@gmail.com>
2023-08-25 13:43:03 -0700

    change

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,7 +16,7 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[len])
 		len++;
 
-	new = (*list_t) malloc(sizeof(list_t));
+	new = (*list_t)malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
