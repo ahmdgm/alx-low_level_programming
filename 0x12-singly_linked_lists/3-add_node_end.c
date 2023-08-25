f478c25e2c31fa425ced45486ec4f502dd47dea8
ahmdgm <ahmedgamalgamal1@gmail.com>
2023-08-25 16:33:17 -0700

    change

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,16 +11,13 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	unsigned int len = 0;
 	list_t *new_node = (list_t *)malloc(sizeof(list_t));
-
-	while (str[len])
-		len++;
 
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->str = strdup(str);
-	new_node->len = len;
+	new_node->len = strlen(str);
 	new_node->next = NULL;
 
 	if (*head == NULL)
@@ -32,9 +29,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *current = *head;
 
 	while (current->next != NULL)
+	{
 		current = current->next;
-	current->next = new_node;
+	}
+
+	 current->next = new_node;
 
 	return (new_node);
-
 }
